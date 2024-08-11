388f93a0e7bac09ffd4ce76f3e2f3983be2d4e93
victor <victorkibichy>
2024-08-11 18:34:02 +0300

    Initial Commit

---
+++ Coin $tratum/Coin__tratumApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coin__tratumApp.swift
+//  Coin $tratum
+//
+//  Created by  Bouncy Baby on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Coin__tratumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
