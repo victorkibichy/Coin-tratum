2907972065b0f80ac39ec4fa07484bcf0dfe175b
victor <victorkibichy>
2024-08-11 19:02:14 +0300

    A custom color extension for the application outlook

---
+++ Coin $tratum/Extensions/Color.swift
@@ -0,0 +1,22 @@
+//
+//  Color.swift
+//  Coin $tratum
+//
+//  Created by  Bouncy Baby on 8/11/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryText")
+}
