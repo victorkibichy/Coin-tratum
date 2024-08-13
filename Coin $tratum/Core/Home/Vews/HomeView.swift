ca33675938213384e768777809e24ff7b02a2a7d
victor <victorkibichy>
2024-08-13 15:59:59 +0300

    code cleanup by refactoring segments

--- Coin $tratum/Core/Home/Vews/HomeView.swift
+++ Coin $tratum/Core/Home/Vews/HomeView.swift
@@ -21,31 +21,7 @@ struct HomeView: View {
             //content layer
             VStack{
                 
-                HStack{
-                    CircleButtonView(iconName: showPortfolio ? "plus" : "info")
-                        .animation(.none)
-                        .background(
-                        CircleButtonAnimationView(animate: $showPortfolio)
-                        )
-                    
-                    Spacer()
-                    
-                    Text(showPortfolio ? "Portfolio" : "Live Prices")
-                        .font(.headline)
-                        .fontWeight(.heavy)
-                        .foregroundColor(Color.theme.accent)
-                        .animation(.none)
-                    
-                    Spacer()
-                    
-                    CircleButtonView(iconName: "chevron.right")
-                        .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
-                        .onTapGesture {
-                            withAnimation(.spring()) {
-                                showPortfolio.toggle()
-                            }
-                        }
-                }
+                homeHeader           
                 
                 Spacer(minLength: 0)
                   
@@ -60,5 +36,38 @@ struct HomeView_Previews: PreviewProvider {
             HomeView()
                 .navigationBarHidden(true)
         }
+    }
+}
+
+extension HomeView  {
+    
+    private var homeHeader: some View {
+        HStack{
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(.none)
+                .background(
+                CircleButtonAnimationView(animate: $showPortfolio)
+                )
+            
+            Spacer()
+            
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none)
+            
+            Spacer()
+            
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+        
     }
 }
