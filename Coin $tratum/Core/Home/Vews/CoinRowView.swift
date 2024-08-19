b1e4056502f8d80913b833098f4ce8c9caa2c9d5
victor <victorkibichy>
2024-08-19 13:23:04 +0300

    view cleanup

--- Coin $tratum/Core/Home/Vews/CoinRowView.swift
+++ Coin $tratum/Core/Home/Vews/CoinRowView.swift
@@ -10,40 +10,71 @@ import SwiftUI
 struct CoinRowView: View {
     
     let coin : CoinModel
+    let showHoldingsColumn: Bool
+    
     
     var body: some View {
         HStack(spacing: 0) {
-            Text("\(coin.rank)")
-                .font(.caption)
-                .foregroundColor(Color.theme.secondaryText)
-                .frame(minWidth: 30)
-            Circle()
-                .frame(width: 30, height: 30)
-            Text(coin.symbol.uppercased())
-                .font(.headline)
-                .padding(.leading, 6)
-                .foregroundColor(Color.theme.accent)
+            leftColumn
             
             Spacer()
-            VStack(alignment: .trailing) {
-                Text("\(coin.currentPrice.asCurrencyWith6Decimals())")
-                    .bold()
-                    .foregroundColor(Color.theme.accent)
-                Text("\(coin.priceChangePercentage24H ?? 0)%")
-                    .foregroundColor(
-                        (coin.priceChangePercentage24H ?? 0) >= 0 ?
-                        Color.theme.green :
-                            Color.theme.red                    )
-
+            if showHoldingsColumn {
+                centerColumn
             }
+            rightColumn
+                .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+
             
         }
+        .font(.subheadline)
         
     }
 }
 
 struct CoinRowView_Previews: PreviewProvider {
     static var previews: some View {
-        CoinRowView(coin: dev.coin)
+        CoinRowView(coin: dev.coin, showHoldingsColumn: true)
     }
+}
+
+extension CoinRowView {
+    private var leftColumn: some View {
+        HStack(spacing: 0) {
+            
+                Text("\(coin.rank)")
+                    .font(.caption)
+                    .foregroundColor(Color.theme.secondaryText)
+                    .frame(minWidth: 30)
+                Circle()
+                    .frame(width: 30, height: 30)
+                Text(coin.symbol.uppercased())
+                    .font(.headline)
+                    .padding(.leading, 6)
+                    .foregroundColor(Color.theme.accent)
+        }
+    }
+    
+    private var centerColumn: some View {
+        VStack(alignment: .trailing){
+            Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
+                .bold()
+            Text((coin.currentHoldings ?? 0).asNumberString())
+        }
+        
+    }
+    private var rightColumn: some View {
+        VStack(alignment: .trailing) {
+            Text("\(coin.currentPrice.asCurrencyWith2Decimals())")
+                .bold()
+                .foregroundColor(Color.theme.accent)
+            Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
+                .foregroundColor(
+                    (coin.priceChangePercentage24H ?? 0) >= 0 ?
+                    Color.theme.green :
+                        Color.theme.red                    )
+
+        }
+        
+    }
+
 }
--- Coin $tratum/Extensions/Double.swift
+++ Coin $tratum/Extensions/Double.swift
@@ -10,11 +10,9 @@ import Foundation
 extension Double {
     
     ///converts a double to a currency with 2 to 6 decimal places
-    /// Converts 1234 to $1,234
-    ///  converts 12.3456 to $12.3456
-    ///  convert 0.123456 to $0.123456
+    /// Converts 1234.56 to $1,234.56
     
-    private var currencyFormatter6: NumberFormatter {
+    private var currencyFormatter2: NumberFormatter {
         let formatter = NumberFormatter()
         formatter.usesGroupingSeparator = true
         formatter.numberStyle = .currency
@@ -22,27 +20,32 @@ extension Double {
 ///        formatter.currencyCode = "usd" // <- change currency
 ///        formatter.currencySymbol = "$" // <- change symbol
         formatter.minimumFractionDigits = 2
-        formatter.maximumFractionDigits = 6
+        formatter.maximumFractionDigits = 2
         
         
         return formatter
     }
     
     ///converts a double to a currency with 2 to 6 decimal places
-    /// Converts 1234 to $1,234
-    ///  converts 12.3456 to $12.3456
-    ///  convert 0.123456 to $0.123456
+    ///  converts 12.3456 to $12.34
     
-    func asCurrencyWith6Decimals() -> String {
+    func asCurrencyWith2Decimals() -> String {
         let number = NSNumber(value: self)
-        return currencyFormatter6.string(from: number) ?? "$0.00"
+        return currencyFormatter2.string(from: number) ?? "$0.00"
     }
     
     ///converts a double to a cstring representation
-    /// Converts 1234.56 to $1,234.56
+    /// Converts 1.234.56 to 1.23
     
     func asNumberString() -> String {
         
         return String(format: "%.2f", self)
+    }
+    
+    ///converts a double to a cstring representation with percent symbol
+    /// Converts 1.234.56 to 1.23%
+  
+    func asPercentString() -> String {
+        return asNumberString() + "%"
     }
 }
