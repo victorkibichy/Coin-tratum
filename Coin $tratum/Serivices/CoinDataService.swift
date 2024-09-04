02c6ac7b9bd31b4e978b78424622919bde8c5071
victor <victorkibichy>
2024-09-04 09:29:41 +0300

    Custom Network Manager

--- Coin $tratum/Serivices/CoinDataService.swift
+++ Coin $tratum/Serivices/CoinDataService.swift
@@ -23,7 +23,8 @@ class CoinDataService {
         else { return }
         
         
-       coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
+       coinSubscription =
+        URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .default))
             .tryMap { (output) -> Data in
                 
---
+++ Coin $tratum/Utilities/NetworkingManager.swift
@@ -0,0 +1,29 @@
+//
+//  NetworkingManager.swift
+//  Coin $tratum
+//
+//  Created by  Bouncy Baby on 9/4/24.
+//
+
+import Foundation
+import Combine
+
+class NetworkingManager {
+    
+    static func download(url: URL) -> Publishers.ReceiveOn<Publishers.TryMap<Publishers.SubscribeOn<URLSession.DataTaskPublisher,DispatchQueue>, Data>, DispatchQueue> {
+     return   URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap { (output) -> Data in
+                
+                guard let response = output.response as? HTTPURLResponse,
+                      response.statusCode >= 200 && response.statusCode < 300 else {
+                    throw URLError(.badServerResponse)
+                }
+                return output.data
+                
+            }
+            .receive(on: DispatchQueue.main)
+        
+    }
+    
+}
