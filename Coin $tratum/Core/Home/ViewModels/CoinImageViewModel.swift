c16dd0357b56737ac1658336e9f05e4a1356fa97
victor <victorkibichy>
2024-09-10 11:48:29 +0300

    Coin images configuration

---
+++ Coin $tratum/Core/Home/ViewModels/CoinImageViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  CoinImageViewModel.swift
+//  Coin $tratum
+//
+//  Created by  Bouncy Baby on 9/9/24.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class CoinImageViewModel: ObservableObject {
+    @Published var image: UIImage? = nil
+    @Published var isLoading: Bool = false
+   
+    private let coin: CoinModel
+    private let dataService: CoinImageService
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        self.dataService = CoinImageService(urlString: coin.image)
+        self.addSubscribers()
+        self.isLoading = true
+    }
+    private func addSubscribers() {
+        dataService.$image
+            .sink { [weak self] (_) in
+                self?.isLoading = false
+            } receiveValue: { [weak self] (returnedImage) in
+                self?.image = returnedImage
+            }
+            .store(in: &cancellables)
+
+        
+    }
+}
---
+++ Coin $tratum/Services/CoinImageService.swift
@@ -0,0 +1,39 @@
+//
+//  CoinImageService.swift
+//  Coin $tratum
+//
+//  Created by  Bouncy Baby on 9/9/24.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class CoinImageService {
+    
+    @Published var image: UIImage? = nil
+    
+    var imageSubscription: AnyCancellable?
+    
+    
+    
+    init(urlString: String) {
+        getCoinImage(urlString: urlString)
+    
+    }
+    private func getCoinImage(urlString: String) {
+        guard let url = URL(string: urlString)
+        else { return }
+        
+        
+        imageSubscription = NetworkingManager.download(url: url)
+            .tryMap({ (data) -> UIImage? in
+                return UIImage(data: data)
+            })
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
+                self?.image = returnedImage
+                self?.imageSubscription?.cancel()
+            })
+        }
+    
+}
