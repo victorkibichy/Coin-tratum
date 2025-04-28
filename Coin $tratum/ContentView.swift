22e5294df5dd9d37788382f5e12b74ac5144521f
Kibichy <victorkibichy@gmail.com>
2025-04-28 14:15:18 +0300

    Currency Formatting

--- Coin $tratum/ContentView.swift
+++ Coin $tratum/ContentView.swift
@@ -1,29 +1,43 @@
+////
+////  ContentView.swift
+////  Coin $tratum
+////
+////  Created by  Bouncy Baby on 8/11/24.
+////
 //
-//  ContentView.swift
-//  Coin $tratum
+//import SwiftUI
 //
-//  Created by  Bouncy Baby on 8/11/24.
+//struct ContentView: View {
+//    var body: some View {
+//        NavigationView {
+//                    ZStack {
+//                        VStack {
+//                            TextField ("Enter your address", text:.constant(""))
+//                               .padding()
+//                               .background(Color.gray.opacity(0.2))
+//                               .cornerRadius(10)
+//                            SecureField("Enter your password", text:.constant(""))
+//                               .padding()
+//                               .background(Color.gray.opacity(0.2))
+//                               .cornerRadius(10)
+//                            NavigationLink(destination: HomeView()) {
+//                                Text("Login")
+//                                   .font(.headline)
+//                                   .foregroundColor(.white)
+//                                   .padding()
+//                                   .background(Color.blue)
+//                                   .cornerRadius(10)
+//                            }
+//                        }
+//                    }
+//                   .padding()
+//                }
+//    }
+//}
 //
-
-import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        ZStack {
-            Color.teal
-            
-                .ignoresSafeArea()
-            
-            VStack {
-                
-            }
-        }
-        .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//            .environmentObject(HomeViewModel())
+//    }
+//}
